f174562c64f0fd26fca31082caf980ba034ed4e9
noural-q <alqassar.n@hotmail.com>
2017-07-04 23:44:47 +0300

    pagination

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render,get_object_or_404,redirect
 from posts.models import *
 from .forms import *
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_create(request):
     form = PostForm(request.POST or None)
@@ -25,12 +26,23 @@ def post_detail(request,post_id):
     return render(request, 'post_detail.html', context)
 
 def post_list(request):
-	object_list = Post.objects.all()
-	context = {
-	"object_list": object_list,
-	"title": "List",
-	}
-	return render(request, 'post_list.html', context)
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 5) # Show 5 contacts per page
+    page = request.GET.get('page')
+    try:
+        objects = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        objects = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        objects = paginator.page(paginator.num_pages)
+    context = {
+    "object_list": objects,
+    "title": "List",
+    "user": request.user
+    }
+    return render(request, 'post_list.html', context)
 
 def post_update(request, post_id):
     instance = get_object_or_404(Post, id=post_id)
