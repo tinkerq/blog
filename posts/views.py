375d83ba712bedb6ea0f348de8f60e6cc75055e4
noural-q <alqassar.n@hotmail.com>
2017-07-06 13:27:22 +0300

    draft handling in post list and post detail pages

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from .forms import *
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from urllib.parse import quote
+from django.utils import timezone
 
 
 
@@ -26,29 +27,36 @@ def post_create(request):
 
 def post_detail(request,post_slug):
     instance = get_object_or_404(Post, slug=post_slug)
+    if instance.publish>timezone.now().date() or instance .draft:
+        if not (request.user.is_staff or request.user.is_superuser):
+            raise Http404
     context = {
-    "instance": instance,
     "title": "Detail",
+    "instance": instance,
     "share_string": quote(instance.content)
     }
     return render(request, 'post_detail.html', context)
 
 def post_list(request):
-    object_list = Post.objects.all()
+    today = timezone.now().date()
+    object_list = Post.objects.filter(draft=False).filter(publish__lte=today)
+    if request.user.is_staff or request.user.is_superuser:
+        object_list = Post.objects.all()
+
     paginator = Paginator(object_list, 5) # Show 5 contacts per page
     page = request.GET.get('page')
     try:
         objects = paginator.page(page)
     except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
         objects = paginator.page(1)
     except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
         objects = paginator.page(paginator.num_pages)
+
     context = {
     "object_list": objects,
     "title": "List",
-    "user": request.user
+    "user": request.user,
+    "today": today
     }
     return render(request, 'post_list.html', context)
 
